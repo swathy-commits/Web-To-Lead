3149f65271832d8bf970d70e043388e970747dd7
swathy-commits <swathy.madanagopal@gmail.com>
2024-11-25 12:21:45 +0530

    Added Validation for ReCaptcha

--- script1.js
+++ script1.js
@@ -1,11 +1,21 @@
-function beforesubmit(){
+let captchaCheck=false;
+function beforesubmit(event){
+    if(captchaCheck)
+    {
     let outputdate=document.querySelector(".outputdate");
     let inputdate=document.querySelector(".inputdate");
     console.log("inputdate: ",inputdate.value)
 
    let formattedDate=new Date(inputdate.value).toLocaleDateString("en-IN");
     outputdate.value=formattedDate; 
-    
+    }
+    else{
+        alert("Please check the ReCAPTCHA box to submit the lead form");
+        //To stop the submission of the form if user not click recaptcha box
+        event.preventDefault();
+
+    }
+}
     function timestamp() { 
         var response = document.getElementById("g-recaptcha-response");
          if (response == null || response.value.trim() == "") {
@@ -15,4 +25,8 @@ function beforesubmit(){
          } 
         } 
         setInterval(timestamp, 500); 
+
+function captchaSuccess()
+{
+    captchaCheck=true;
 }
